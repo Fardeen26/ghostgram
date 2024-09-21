bd6acd366b19831fad0821cc247c9917e6878455
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-09-21 15:35:20 +0530

    Navbar created

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "../globals.css";
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster"
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   // src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
     <html lang="en">
       <AuthProvider>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Navbar />
         {children}
         <Toaster />
       </body>

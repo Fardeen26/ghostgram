bd3c21751a98b625af323a1aa94a5fbc405f96ce
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-10-02 16:01:03 +0530

    configured environment variables

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -15,7 +15,7 @@ async function dbConnect(): Promise<void> {
 
   try {
     // Attempt to connect to the database
-    const db = await mongoose.connect('mongodb+srv://fardeenmansuri0316:yfyo9PgBg1DFfhdq@cluster0.dfo5x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {});
+    const db = await mongoose.connect(process.env.MONGODB_URL as string, {});
 
     connection.isConnected = db.connections[0].readyState;
 

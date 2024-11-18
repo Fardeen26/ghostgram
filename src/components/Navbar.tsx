f4cfa7dc3012e087afbbd943a394d1ae7e8da94c
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-18 15:44:14 +0530

    fix: navbar ui changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,9 +13,9 @@ function Navbar() {
   const user: User = session?.user;
 
   return (
-    <nav className="p-4 md:p-6 shadow-md text-white">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <a href="/" className={`text-2xl font-bold mb-4 md:mb-0 flex space-x-[2px] ${bricolage_grotesque}`}>
+    <nav className={`p-4 md:p-6 shadow-md text-white ${bricolage_grotesque}`}>
+      <div className="container mx-auto flex md:flex-row justify-between items-center">
+        <a href="/" className={`text-2xl font-bold mb-4 md:mb-0 flex space-x-[2px]`}>
           <span>
             <Image src={'/ghostgram-logo.png'} alt='logo' height={30} width={30} className='rounded-xl' />
           </span>
@@ -25,16 +25,13 @@ function Navbar() {
         </a>
         {session ? (
           <>
-            <span className="mr-4">
-              Welcome, {user.username || user.email}
-            </span>
-            <Button onClick={() => signOut()} className="w-full md:w-auto bg-slate-100 text-black" variant='outline'>
+            <Button onClick={() => signOut()} className="w-full md:w-auto bg-white text-black rounded-full px-8 h-8" variant='outline'>
               Logout
             </Button>
           </>
         ) : (
           <Link href="/sign-in">
-            <Button className="w-full px-7 h-9 bg-white text-black rounded-full" variant={'outline'}>Login</Button>
+            <Button className="w-full bg-white text-black rounded-full px-8 h-8" variant={'outline'}>Login</Button>
           </Link>
         )}
       </div>

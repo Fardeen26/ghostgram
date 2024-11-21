627e1ee54b5ff22f2fd6835e551983e464d31027
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-11-21 13:34:49 +0530

    fix: replaced openai with gemini

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,36 +1,47 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
-import { NextResponse } from 'next/server';
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-export const runtime = 'edge';
+const randomThemes = [
+  "hobbies",
+  "travel",
+  "personal growth",
+  "entertainment",
+  "coding",
+  "programming",
+  "future goals",
+  "books",
+  "relationships",
+  "cricket",
+  "memes",
+  "personal questions"
+];
 
-export async function POST(req: Request) {
+export async function GET() {
   try {
-    const prompt =
-      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+    const randomTheme =
+      randomThemes[Math.floor(Math.random() * randomThemes.length)];
 
-    const response = await openai.completions.create({
-      model: 'gpt-3.5-turbo-instruct',
-      max_tokens: 400,
-      stream: true,
-      prompt,
-    });
+    const prompt = `Create a list of three unique and short question words range 10-15, open-ended, and engaging questions focusing on the theme of "${randomTheme}". Each question should be separated by '||'. These questions are for an anonymous social messaging platform like Qooh.me. Avoid personal or sensitive topics, and ensure the questions encourage interaction, curiosity, and positivity.`;
 
-    const stream = OpenAIStream(response);
+    const result = await model.generateContent(prompt);
 
+    return Response.json(
+      {
+        success: true,
+        message: result.response.text(),
+      },
+      { status: 200 }
+    );
 
-    return new StreamingTextResponse(stream);
   } catch (error) {
-    if (error instanceof OpenAI.APIError) {
-      const { name, status, headers, message } = error;
-      return NextResponse.json({ name, status, headers, message }, { status });
-    } else {
-      console.error('An unexpected error occurred:', error);
-      throw error;
-    }
+    return Response.json(
+      {
+        success: true,
+        message: "Error while generating the messages!",
+      },
+      { status: 501 }
+    );
   }
 }
